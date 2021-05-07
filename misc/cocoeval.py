9d011b42a8019bc286922d3f65f56dc4287200c5
CarolinaCheng616 <948005247@qq.com>
2021-05-07 17:50:51 +0800

    pdb

--- misc/cocoeval.py
+++ misc/cocoeval.py
@@ -58,6 +58,7 @@ class COCOScorer(object):
             gts[ID] = GT[ID]
             res[ID] = RES[ID]
         print('tokenization...')
+        import pdb; pdb.set_trace()
         tokenizer = PTBTokenizer()
         gts = tokenizer.tokenize(gts)
         res = tokenizer.tokenize(res)
