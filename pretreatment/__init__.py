563f90c5ca83cbb263123597c357986871bf1b46
CarolinaCheng616 <948005247@qq.com>
2021-05-08 23:15:57 +0800

    init

--- pretreatment/__init__.py
+++
@@ -1,9 +0,0 @@
-from .extract_frames_from_videos import extract_frames
-from .extract_image_feats_from_frames import extract_feats
-from .resnet import ResNet
-
-__all__ = [
-    "extract_frames",
-    "extract_feats",
-    "ResNet"
-]
